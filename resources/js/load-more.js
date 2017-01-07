b2d49e006a74007e038cecf93eb1c25909ccacbd
Craig Horsborough <craighorsborough@gmail.com>
2017-01-07 23:45:03 +0000

    updated to show mide things and not load any more if max pages reached

--- resources/js/load-more.js
+++ resources/js/load-more.js
@@ -1,11 +1,27 @@
 (function(){
   var loadMoreButton = document.getElementById('load-button');
-  var pageNumber = 1;
   var Helpers = new HelperFunctions;
+  var loadingImage = document.getElementById('loading-image');
+  var noMorePostsElement = document.getElementById('no-more-pages');
+
+  setTimeout(function (){
+    if (loadingImage) {
+      loadingImage.src = loadingImagePath;
+    }
+  });
 
   loadMoreButton.onclick = function () {
+    if (Helpers.totalPagesReached) {
+      Helpers.displayNoMorePagesMessage();
+      return;
+    }
+    Helpers.showLoadingImage();
     var request = new XMLHttpRequest(),
         nextPageUrl = Helpers.getNextUrl();
+
+        if (!nextPageUrl) {
+          return;
+        }
 
     request.addEventListener("load", Helpers.requestListener);
     request.open("GET", nextPageUrl);
@@ -13,6 +29,9 @@
   }
 
   function HelperFunctions () {
+    this.pageNumber = 1;
+    this.totalPagesReached = false;
+
     this.requestListener = function (responseData) {
       var parser = new window.DOMParser(),
           parsedXml = parser.parseFromString(responseData.target.responseText, "text/xml"),
@@ -20,13 +39,46 @@
           currentPageContent = document.getElementById('left-main-content');
 
       currentPageContent.appendChild(nextPageContent);
+
+      Helpers.hideLoadingImage();
     }
 
     this.getNextUrl = function () {
-      pageNumber++;
-      var nextPageUrl = nextUrl.substring(0, nextUrl.length - 1) + pageNumber;
+      this.pageNumber++;
+      var nextPageUrl = nextUrl.substring(0, nextUrl.length - 1) + this.pageNumber;
+
+      if (this.pageNumber === totalNumberOfPages) {
+        this.totalPagesReached = true;
+      }
+
+      if (this.pageNumber > totalNumberOfPages) {
+        return;
+      }
 
       return nextPageUrl;
+    }
+
+    this.displayNoMorePagesMessage = function () {
+      if (!noMorePostsElement) {
+        return;
+      }
+
+      loadMoreButton.style.display = "none";
+      noMorePostsElement.style.display = "block";
+    }
+
+    this.showLoadingImage = function () {
+      if (!loadingImage) {
+        return;
+      }
+      loadingImage.style.display = "block";
+    }
+
+    this.hideLoadingImage = function () {
+      if (!loadingImage) {
+        return;
+      }
+      loadingImage.style.display = "none";
     }
   }
 })();

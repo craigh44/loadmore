102d72ee28d255929284b2a3373a0df71d05cf9d
Craig Horsborough <craighorsborough@gmail.com>
2017-01-07 22:25:43 +0000

    increments page numbers, for some reason any number seems to return stuff...

--- resources/js/load-more.js
+++ resources/js/load-more.js
@@ -1,19 +1,32 @@
 (function(){
   var loadMoreButton = document.getElementById('load-button');
-
-  function requestListener (responseData) {
-    var parser = new window.DOMParser(),
-        parsedXml = parser.parseFromString(responseData.target.responseText, "text/xml"),
-        nextPageContent = parsedXml.getElementById('left-main-content'),
-        currentPageContent = document.getElementById('left-main-content');
-
-    currentPageContent.appendChild(nextPageContent);
-  }
+  var pageNumber = 1;
+  var Helpers = new HelperFunctions;
 
   loadMoreButton.onclick = function () {
-    var request = new XMLHttpRequest();
-    request.addEventListener("load", requestListener);
-    request.open("GET", nextUrl);
+    var request = new XMLHttpRequest(),
+        nextPageUrl = Helpers.getNextUrl();
+
+    request.addEventListener("load", Helpers.requestListener);
+    request.open("GET", nextPageUrl);
     request.send();
+  }
+
+  function HelperFunctions () {
+    this.requestListener = function (responseData) {
+      var parser = new window.DOMParser(),
+          parsedXml = parser.parseFromString(responseData.target.responseText, "text/xml"),
+          nextPageContent = parsedXml.getElementById('left-main-content'),
+          currentPageContent = document.getElementById('left-main-content');
+
+      currentPageContent.appendChild(nextPageContent);
+    }
+
+    this.getNextUrl = function () {
+      pageNumber++;
+      var nextPageUrl = nextUrl.substring(0, nextUrl.length - 1) + pageNumber;
+
+      return nextPageUrl;
+    }
   }
 })();
